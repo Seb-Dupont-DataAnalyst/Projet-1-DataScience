22db993f293e74767112dd8c0dd1739dfcde45ad
Sebastien Dupont <79512309+Seb-Dupont-DataAnalyst@users.noreply.github.com>
2021-11-26 11:11:17 +0100

    Update Projet_1.py

--- Projet_1.py
+++ Projet_1.py
@@ -61,7 +61,7 @@ if choice == "Accueil":
     st.write("Bonjour, bienvenue pour cette présentation....")
     st.title("")
     
-    col1, col2 = st.beta_columns(2)
+    col1, col2 = st.columns(2)
     with col1:
       st.image("https://miro.medium.com/max/1400/1*3zdEDGlAT5WEpOEbUj_luA.jpeg")
     with col2:
