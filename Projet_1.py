34202182eb21e2320203cb271ac554678a5f8953
Sebastien Dupont <79512309+Seb-Dupont-DataAnalyst@users.noreply.github.com>
2021-11-26 11:11:53 +0100

    Update Projet_1.py

--- Projet_1.py
+++ Projet_1.py
@@ -65,7 +65,7 @@ if choice == "Accueil":
     with col1:
       st.image("https://miro.medium.com/max/1400/1*3zdEDGlAT5WEpOEbUj_luA.jpeg")
     with col2:
-      st.image("http://www.usbeacon.com/images/Iowa/maps/Ames_o.gif")
+      st.image("http://www.usbeacon.com/images/Iowa/maps/Ames_o.gif", width = 400)
     
     
 
