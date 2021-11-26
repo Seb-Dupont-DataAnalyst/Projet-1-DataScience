63a9b172b0e2f3c13315b69b32b62285f7664764
Sebastien Dupont <79512309+Seb-Dupont-DataAnalyst@users.noreply.github.com>
2021-11-26 12:50:56 +0100

    Update Projet_1.py

--- Projet_1.py
+++ Projet_1.py
@@ -185,7 +185,7 @@ if choice == "Analyse Exploratoire":
 
 
 
-        code = ("""
+    code = ("""
         1Fam    Single-family Detached
         2FmCon  Two-family Conversion; originally built as one-family dwelling
         Duplx   Duplex
